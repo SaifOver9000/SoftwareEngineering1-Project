b232f90d2726278818fac92d8d3d144fd7eb3b60
SaifOver9000 <123909641+SaifOver9000@users.noreply.github.com>
2023-11-30 12:52:45 -0400

    Update Main.java

--- src code/Main.java
+++ src code/Main.java
@@ -1,117 +1,119 @@
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
+/**
+ * Main class to run the Wolfville store application.
+ */
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Welcome to the Wolfville store");
+            System.out.println("Please select between our 3 options");
+            System.out.println("1 -- to buy and checkout");
+            System.out.println("2 -- to return");
+            System.out.println("3 -- to join our special program");
+            System.out.print("Enter your choice (1, 2, or 3): ");
 
-public class Main 
-{
-    public static void main(String[] args) 
-    {
-        Scanner input = new Scanner(System.in);
-        int choice;
+            int choice = input.nextInt();
 
-        System.out.println("Welcome to the Wolfville store");
-        System.out.println("Please select between our 3 options");
+            Map<Integer, CProduct> products = CProduct.getAllCProducts();
+            CLoyaltyCardDetails loyaltyCardDetails = new CLoyaltyCardDetails();
 
-        System.out.println("1 -- to buy and checkout");
-        System.out.println("2 -- to Return");
-        System.out.println("3 -- to join our Special program");
-
-        System.out.println("Enter your choice (1, 2 or 3): ");
-
-        choice = input.nextInt();
-
-        while (choice < 1 || choice > 3) {
-            System.out.println("Please enter either 1, 2 or 3");
-            choice = input.nextInt();
-        }
-
-        Map<Integer, CProduct> products = CProduct.getAllCProducts(); // Create a map of products
-        CLoyaltyCardDetails loyaltyCardDetails = new CLoyaltyCardDetails(); // Create an instance of LoyaltyCardDetails
-    
-
-        switch (choice) 
-        {
-            case 1:
-                CCheckoutSystem checkoutSystem = new CCheckoutSystem(products, loyaltyCardDetails);
-                checkoutSystem.checkout();
-                break;
-
-            case 2:
-                processReturns(products);
-                break;
-
-            case 3:
-                joinLoyaltyProgram(loyaltyCardDetails);
-                break;
-                
-        }
-        input.close();
-    }
-
-    public static void processReturns(Map<Integer, CProduct> products) 
-    {   
-        double totalReturnedAmount = 0.0;
-        Scanner input = new Scanner(System.in);
-        System.out.println("Please enter the Serial number of the item you want to return (-1 to finish): ");
-        
-        while (true) 
-        {   int serialNumber = input.nextInt();
-
-            if (serialNumber == -1) 
-            {
-                break;
+            switch (choice) {
+                case 1:
+                    CCheckoutSystem checkoutSystem = new CCheckoutSystem(products);
+                    checkoutSystem.checkout();
+                    break;
+                case 2:
+                    processReturns(input, products);
+                    break;
+                case 3:
+                    joinLoyaltyProgram(input, loyaltyCardDetails);
+                    break;
+                default:
+                    System.out.println("Invalid choice.");
             }
-            if (products.containsKey(serialNumber)) 
-            {
-                CReturns returnHandler = new CReturns(0, 0);
-                double returnedAmount = returnHandler.makeReturn(serialNumber, products);
-                System.out.println("amount returned: $" + returnedAmount);
-                totalReturnedAmount += returnedAmount;
-            } 
-            else 
-            {
-                System.out.println("Product not found with the given serial number.");
+
+        } catch (InputMismatchException exception) {
+            System.out.println("Invalid input. Please enter a valid choice (1, 2, or 3).");
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Process returns for purchased items.
+     *
+     * @param input    Scanner for user input
+     * @param products Map of available products
+     */
+    private static void processReturns(Scanner input, Map<Integer, CProduct> products) {
+        DecimalFormat currencyFormat = new DecimalFormat("$#.00");
+        System.out.println("Available Products:");
+        for (Map.Entry<Integer, CProduct> entry : products.entrySet()) {
+            System.out.println("Serial Number: " + entry.getKey() + ", Name: " +
+                    entry.getValue().getName() + ", Price: " +
+                    currencyFormat.format(entry.getValue().getPrice()));
+        }
+        System.out.println("Enter the serial number of the item you want to return (-1 to finish):");
+        CReturns returns = new CReturns();
+        double totalAmtReturned = 0;
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+
+        while (true) {
+            try {
+                int serialNumber = input.nextInt();
+                if (serialNumber == -1) {
+                    break;
+                }
+                double returnedAmount = returns.makeReturn(serialNumber, products);
+                totalAmtReturned += returnedAmount;
+                System.out.println("Amount returned: $" + decimalFormat.format(returnedAmount));
+            } catch (InputMismatchException inputException) {
+                System.out.println("Invalid input. Please enter a valid serial number.");
+                input.next();
+            } catch (Exception e) {
+                System.out.println("An error occurred: " + e.getMessage());
             }
         }
-
-        System.out.println("Total amount returned: $" + totalReturnedAmount);
-        input.close();
+        System.out.println("Total Amount Returned: $" + decimalFormat.format(totalAmtReturned));
     }
 
-    public static void joinLoyaltyProgram(CLoyaltyCardDetails loyaltyCardDetails)  
-    {
-        // Implement the logic for joining the loyalty program
-        Scanner input = new Scanner(System.in);
+    /**
+     * Join the loyalty program by entering a unique card number and code.
+     *
+     * @param input              Scanner for user input
+     * @param loyaltyCardDetails Loyalty card details manager
+     */
+    private static void joinLoyaltyProgram(Scanner input, CLoyaltyCardDetails loyaltyCardDetails) {
+        try {
+            System.out.println("Please enter a unique card number (4 digits):");
+            int cardNumber = input.nextInt();
 
-        System.out.println("Please enter a unique card number you want to have: ");
-        String card_number = input.nextLine();
+            if (String.valueOf(cardNumber).length() != 4) {
+                System.out.println("Invalid card number length. Please enter a 4-digit number.");
+                return;
+            }
 
-        System.out.println("Please enter a unique code  you want to have: ");
-        int code = input.nextInt();
+            System.out.println("Please enter a unique code (4 digits):");
+            int code = input.nextInt();
 
-        // Write the loyalty card information to a file
-        writeLoyaltyCardToFile(card_number, code);
+            if (String.valueOf(code).length() != 4) {
+                System.out.println("Invalid code length. Please enter a 4-digit number.");
+                return;
+            }
 
-        loyaltyCardDetails.addCard(card_number, code);
-        System.out.println("You have successfully joined the loyalty program.");
-        input.close();
-    }
-    private static void writeLoyaltyCardToFile(String cardNumber, int code) 
-    {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("LoyaltyProgramMembers.txt", true))) 
-        {
-            // Append the loyalty card information to the file
-            writer.write(cardNumber + "," + code);
-
-            writer.newLine();  // Add a new line for the next entry
-        } 
-        catch (IOException e) 
-
-        {
-            e.printStackTrace();
+            loyaltyCardDetails.addCard(String.valueOf(cardNumber), code);
+            loyaltyCardDetails.writeCardDetailsToFile("loyaltyMembers.txt");
+            System.out.println("You have successfully joined the loyalty program.");
+        } catch (InputMismatchException inputexception) {
+            System.out.println("Invalid input. Please enter valid integers.");
+            input.next();
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
         }
     }
 }
