412f807204abbc4c2ff0542189576e67cb972ee1
SaifOver9000 <123909641+SaifOver9000@users.noreply.github.com>
2023-11-30 12:49:01 -0400

    Update CLoyaltyCardDetails.java

--- src code/CLoyaltyCardDetails.java
+++ src code/CLoyaltyCardDetails.java
@@ -1,30 +1,80 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Represents details related to loyalty cards, including card numbers and unique codes.
-*/
-public class CLoyaltyCardDetails 
+ * Class representing loyalty card details in the Wolfville store.
+ */
+public class CLoyaltyCardDetails {
 
-{
-    // Map to store card details (card number -> unique code)
-    private Map<String, Integer> _cardDetails;
+    private static Map<String, Integer> cardDetails;
 
     /**
-     * Constructs a loyalty card details object with an empty map.
+     * Constructor for CLoyaltyCardDetails. Initializes cardDetails and loads details from a file.
      */
     public CLoyaltyCardDetails() {
-        this._cardDetails = new HashMap<>();
+        CLoyaltyCardDetails.cardDetails = new HashMap<>();
+        loadCardDetailsFromFile("loyaltyMembers.txt");
     }
 
     /**
-     * Adds a loyalty card to the details with the provided card number and unique code.
+     * Adds a new loyalty card with the given card number and unique code.
      *
-     * @param cardNumber The card number to be added
+     * @param cardNumber The card number
      * @param uniqueCode The unique code associated with the card
      */
     public void addCard(String cardNumber, int uniqueCode) {
-        _cardDetails.put(cardNumber, uniqueCode);
+        cardDetails.put(cardNumber, uniqueCode);
     }
 
+    /**
+     * Loads loyalty card details from a file and populates the cardDetails map.
+     *
+     * @param fileName The name of the file containing loyalty card details
+     */
+    private void loadCardDetailsFromFile(String fileName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length == 2) {
+                    cardDetails.put(parts[0].trim(), Integer.parseInt(parts[1].trim()));
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error reading loyalty card details file: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Writes loyalty card details to a file.
+     *
+     * @param fileName The name of the file to write loyalty card details
+     */
+    public void writeCardDetailsToFile(String fileName) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true))) {
+            for (Map.Entry<String, Integer> entry : cardDetails.entrySet()) {
+                writer.write(entry.getKey() + "," + entry.getValue());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.err.println("Error writing loyalty card details to file: " + e.getMessage());
+        }
+    }
+
+    /**
+     * Checks if a loyalty card with the given card number and code is a valid member.
+     *
+     * @param cardNumber The card number
+     * @param code       The code associated with the card
+     * @return True if the card is a valid member, false otherwise
+     */
+    public static boolean isValidMember(String cardNumber, int code) {
+        Integer storedCode = cardDetails.get(cardNumber);
+        return storedCode != null && storedCode == code;
+    }
 }
